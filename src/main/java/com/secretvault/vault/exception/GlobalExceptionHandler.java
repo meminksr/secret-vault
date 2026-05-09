df17b823fe5f92a3c2e2031c664c7fff8ac33572
mehmet <mehmetkeser@gmail.com>
2026-05-09 13:20:20 +0300

    add global exception handler for clean error responses

--- src/main/java/com/secretvault/vault/exception/GlobalExceptionHandler.java
+++ src/main/java/com/secretvault/vault/exception/GlobalExceptionHandler.java
@@ -1,11 +1,9 @@
 package com.secretvault.vault.exception;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,8 +15,8 @@ public class GlobalExceptionHandler {
         // Tertemiz bir JSON formatı
         Map<String, Object> errorResponse = new HashMap<>();
 
-        errorResponse.put("durum", "BAŞARISIZ");
-        errorResponse.put("kod", 400);
+        errorResponse.put("status", "FAILED");
+        errorResponse.put("code", 400);
 
         // Entity'de @NotBlank içine yazdığımız o özel mesajı cımbızla çekiyoruz
         String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
